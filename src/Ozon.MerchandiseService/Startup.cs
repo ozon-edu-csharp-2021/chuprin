b7a1408831a72622b78fd4747fd56af4a9e60333
Lev Chuprin <leva.chuprin@mail.ru>
2021-11-14 17:18:06 +0300

    - Убрана ошибка ссылки на namespace

--- src/Ozon.MerchandiseService/Startup.cs
+++ src/Ozon.MerchandiseService/Startup.cs
@@ -1,26 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Ozon.MerchandiseService.Domain.AggregateModels.EmployeeAggregate;
-using Ozon.MerchandiseService.Domain.AggregateModels.MerchIssueAggregate;
 using Ozon.MerchandiseService.GrpcService;
-using Ozon.MerchandiseService.Infrastructure;
-using Ozon.MerchandiseService.Infrastructure.Commands;
-using Ozon.MerchandiseService.Infrastructure.Filters;
-using Ozon.MerchandiseService.Infrastructure.Handlers;
-using Ozon.MerchandiseService.Infrastructure.Middlewares;
-using Ozon.MerchandiseService.Infrastructure.Repositories;
-using Ozon.MerchandiseService.Infrastructure.StartupFilters;
-using Ozon.MerchandiseService.Services.Interfaces;
 
 namespace Ozon.MerchandiseService
 {
